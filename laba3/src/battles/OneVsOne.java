2fdf4adbd888ed4de3ff0fa33f4ed3e5be7ff584
Your Name <apiskin557@gmail.com>
2022-09-16 23:33:51 +0300

    fill class - OneVsOne

--- laba3/src/battles/OneVsOne.java
+++ laba3/src/battles/OneVsOne.java
@@ -1,4 +1,34 @@
 package battles;
 
+import druids.BasicDroid;
+
 public class OneVsOne {
+
+    private BasicDroid robWhite, robBlack;
+
+    public OneVsOne(BasicDroid r1, BasicDroid r2){
+        robWhite = r1;
+        robBlack = r2;
+    }
+
+    public void battle(){
+        robWhite.makeMove(robBlack);
+        if (anybodyWon()) return;
+
+        robBlack.makeMove(robWhite);
+        if (anybodyWon()) return;
+
+        battle();
+    }
+
+    private boolean anybodyWon(){
+        if (robWhite.isDied() || robBlack.isDied()){
+            boolean whiteWinner = robBlack.isDied();
+            System.out.println((whiteWinner ? (robWhite.toString()+ " (Біла ") : (robBlack.toString() + " (Чорна ")) +
+                    "команда) - перемога");
+            return true;
+        }
+        return false;
+    }
+
 }
